d78d9a28f93781d29d0bacc6d6c0a1c4d183aacd
vaishnavmore2004 <morevaishnav70@gmail.com>
2025-07-30 23:42:03 +0530

    Updated 2.0

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -49,7 +49,7 @@ const AppShowcase = () => {
         <div className="showcaselayout">
           <div ref={rydeRef} className="first-project-wrapper">
             <div className="image-wrapper">
-              <img src="/images/INTE.png" alt="Ryde App Interface" />
+              <img src="/images/IBSpic.png" alt="Ryde App Interface" />
             </div>
             <div className="text-content">
               <h2>
